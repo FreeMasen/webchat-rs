4c58ca0896f54cd91cf2d8834afae87b8d436cc5
Robert Masen <r@robertmasen.pizza>
2018-04-12 21:39:12 -0500

    switch ser/de impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,22 @@
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate bincode;
+extern crate rmp_serde;
 
-use bincode::{deserialize as bin_de, serialize as bin_ser, Error};
-
+use rmp_serde::encode::Error as EncodeError;
+use rmp_serde::decode::Error as DecodeError;
+use rmp_serde::{from_slice, to_vec};
 #[derive(Serialize, Deserialize,Debug,PartialEq)]
 pub enum Message {
     Ping, Pong, Chat(String), Nick(String), Me(String)
 }
 
-pub fn serialize(message: Message) -> Result<Vec<u8>, Error> {
-    bin_ser(&message)
+pub fn serialize(message: Message) -> Result<Vec<u8>, EncodeError> {
+    to_vec(&message)
 }
 
-pub fn deserialize(buffer: &[u8]) -> Result<Message, Error> {
-    bin_de(buffer)
+pub fn deserialize(buffer: &[u8]) -> Result<Message, DecodeError> {
+    from_slice(buffer)
 }
 
 
